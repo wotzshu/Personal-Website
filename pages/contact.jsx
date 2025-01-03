6b302c9432d43a66bed6511b6b488676ee3f72a7
wotzshu <wotzshuofficial@gmail.com>
2025-01-03 23:46:38 +0300

    Update contact.jsx

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -10,32 +10,26 @@ export default function Home() {
   return (
     <>
       <div className="py-20">
-        <p className="text-3xl text-white font-semibold text-center">Contact Me</p>
+        <p className="text-3xl text-white font-semibold text-center">Bana ulaş</p>
         <p className="text-xl text-white/50 font-normal text-center mb-5">Other addresses where you can contact me.</p>
         <div className=" flex justify-center flex-col items-center">
             <div className="max-w-lg w-full">
-            <a target="_blank" rel="norefferer" href="https://discord.com/users/518341959765458985">
-                    <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
-                        <i className="fab fa-discord fa-2x" />
-                        <p className="font-semibold text-xl">@umutxyp</p>
-                    </div>
-                </a>
-                <a target="_blank" rel="norefferer" href="https://instagram.com/umutxyp">
+                <a target="_blank" rel="norefferer" href="https://instagram.com/wotzshu/">
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fab fa-instagram fa-2x" />
-                        <p className="font-semibold text-xl">@umutxyp</p>
+                        <p className="font-semibold text-xl">@wotzshu</p>
                     </div>
                 </a>
-                <a target="_blank" rel="norefferer" href="https://t.me/umutxyp">
+                <a target="_blank" rel="norefferer" href="xxx">
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
-                        <i className="fab fa-telegram fa-2x" />
-                        <p className="font-semibold text-xl">@umutxyp</p>
+                        <i className="fab fa-youtube fa-2x" />
+                        <p className="font-semibold text-xl">@wotzshu</p>
                     </div>
                 </a>
-                <a href="mailto:umutbayraktar55@gmail.com">
+                <a href="mailto:wotzshuofficial@gmail.com">
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fa fa-envelope fa-2x" />
-                        <p className="font-semibold text-xl">umutbayraktar55@hotmail.com</p>
+                        <p className="font-semibold text-xl">wotzshuofficial@gmail.com</p>
                     </div>
                 </a>
             </div>
